9acb9e9358f3abfa7abeed1da7916a7e65a4ff6b
Topi Talvitie <topi.talvitie@iki.fi>
2021-12-02 00:36:21 +0200

    Improve test script.

--- test.sh
+++ test.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 set -e
+set -o pipefail
 
 for i in {1..1}
 do
-    echo "Day $i debug"
+    echo "--- Day $i debug"
     ./aoc2021.debug $i < data/input$i.txt | diff - data/output$i.txt
-    echo "Day $i release"
+    echo "--- Day $i release"
     ./aoc2021.release $i < data/input$i.txt | diff - data/output$i.txt
 done
 
-echo "Tests passed"
+echo "--- Tests passed"
