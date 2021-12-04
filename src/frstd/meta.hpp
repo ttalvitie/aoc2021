7268618186f76e815bd8a4c431e01bb52e63859f
Topi Talvitie <topi.talvitie@iki.fi>
2021-12-04 17:43:34 +0200

    Implement perfect forwarding.

--- src/frstd/meta.hpp
+++ src/frstd/meta.hpp
@@ -2,24 +2,40 @@
 
 namespace frstd {
 
+namespace meta_ {
+
 template <typename T>
-struct RemoveReference {
+struct RemoveReferenceImpl {
     using Type = T;
 };
 
 template <typename T>
-struct RemoveReference<T&> {
+struct RemoveReferenceImpl<T&> {
     using Type = T;
 };
 
 template <typename T>
-struct RemoveReference<T&&> {
+struct RemoveReferenceImpl<T&&> {
     using Type = T;
 };
 
+}
+
 template <typename T>
-typename RemoveReference<T>::Type&& move(T&& obj) {
-    return (typename RemoveReference<T>::Type&&)obj;
+using RemoveReference = typename meta_::RemoveReferenceImpl<T>::Type;
+
+template <typename T>
+RemoveReference<T>&& move(T&& obj) {
+    return (RemoveReference<T>&&)obj;
+}
+
+template <typename T>
+T&& forward(RemoveReference<T>& obj) {
+    return (T&&)obj;
+}
+template <typename T>
+T&& forward(RemoveReference<T>&& obj) {
+    return (T&&)obj;
 }
 
 }
