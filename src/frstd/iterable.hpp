0179eed09ee44e6dab434164a8c2ecffa82553ba
Topi Talvitie <topi.talvitie@iki.fi>
2021-12-10 20:16:35 +0200

    Implement range-for adapters for iterators.

--- src/frstd/iterable.hpp
+++ src/frstd/iterable.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <frstd/baseutil.hpp>
+#include <frstd/meta.hpp>
+
 namespace frstd {
 
 class Iterable {};
@@ -14,11 +17,83 @@ NotIterableMarker testFunc(void*);
 
 }
 
-template <typename T, typename = decltype(iterator_::testFunc((T*)nullptr))>
+template <typename T, typename = decltype(iterator_::testFunc((RemoveReference<T>*)nullptr))>
 struct IsIterable {};
 template <typename T>
 struct IsIterable<T, iterator_::IterableMarker> {
     using Yes = int;
 };
+
+class RangeForEndIteratorAdapter {};
+template <typename T>
+class RangeForIteratorAdapter {
+public:
+    RangeForIteratorAdapter(T iterator) : iterator_(move(iterator)) {}
+
+    RangeForIteratorAdapter(const RangeForIteratorAdapter&) = delete;
+    RangeForIteratorAdapter(RangeForIteratorAdapter&&) = delete;
+    RangeForIteratorAdapter& operator=(const RangeForIteratorAdapter&) = delete;
+    RangeForIteratorAdapter& operator=(RangeForIteratorAdapter&&) = delete;
+
+    decltype(auto) operator*() {
+#ifdef FRSTD_DEBUG
+        if(!ready_) {
+            baseutil::fail("FAIL: Call to range for iterator adapter operator* not allowed in this state");
+        }
+        ready_ = false;
+#endif
+        return iterator_.next();
+    }
+    RangeForIteratorAdapter<T>& operator++() {
+#ifdef FRSTD_DEBUG
+        if(ready_) {
+            baseutil::fail("FAIL: Call to range for iterator adapter operator++ not allowed in this state");
+        }
+        ready_ = true;
+#endif
+        return *this;
+    }
+
+private:
+    T iterator_;
+#ifdef FRSTD_DEBUG
+    bool ready_ = true;
+#endif
+
+    template <typename T2>
+    friend bool operator!=(const RangeForIteratorAdapter<T2>& a, const RangeForEndIteratorAdapter&);
+};
+
+template <typename T>
+bool operator!=(const RangeForIteratorAdapter<T>& a, const RangeForEndIteratorAdapter&) {
+    return a.iterator_.hasNext();
+}
+
+template <typename T, typename = VoidT<typename IsIterable<T>::Yes>>
+auto begin(T&& obj) {
+    return RangeForIteratorAdapter(createIterator(forward<T>(obj)));
+}
+template <typename T, typename = VoidT<typename IsIterable<T>::Yes>>
+RangeForEndIteratorAdapter end(T&&) {
+    return RangeForEndIteratorAdapter();
+}
+
+template <typename T>
+struct RangeForConsumeWrapper {
+    T obj;
+};
+template <typename T>
+RangeForConsumeWrapper<RemoveReference<T>> consume(T&& obj) {
+    return {move(obj)};
+}
+
+template <typename T, typename = VoidT<typename IsIterable<T>::Yes>>
+auto begin(RangeForConsumeWrapper<T>& wrapper) {
+    return begin(move(wrapper.obj));
+}
+template <typename T, typename = VoidT<typename IsIterable<T>::Yes>>
+RangeForEndIteratorAdapter end(RangeForConsumeWrapper<T>&) {
+    return RangeForEndIteratorAdapter();
+}
 
 }
